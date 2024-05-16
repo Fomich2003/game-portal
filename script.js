a0a1cb0720147b6025015aa881bf1ff7eae0298d
Fomich2003 <fomichdima1a@gmail.com>
2024-05-16 18:29:33 +0300

    mobile version

--- script.js
+++ script.js
@@ -82,3 +82,16 @@ function random () {
    randomp.innerHTML=gamesImg[number].title
 }
 randombtn.addEventListener('click', random)
+
+
+//burger
+
+let burgerBtn = document.querySelector('.burger')
+let navTag = document.querySelector('nav')
+
+burgerBtn.addEventListener('click', toggleNav)
+
+function toggleNav () {
+    burgerBtn.classList.toggle('active')
+    navTag.classList.toggle('active')
+}
